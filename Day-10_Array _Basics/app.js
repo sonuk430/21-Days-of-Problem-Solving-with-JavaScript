b70f9a9217f3be16c81758a0d487d4388ec84b45
Sonu Kumar <kumarsonuk430@gmail.com>
2026-03-24 21:01:19 +0530

    array

---
+++ Day-10_Array _Basics/app.js
@@ -0,0 +1,21 @@
+//! Find the max and min element
+// const arr = [12, 47, 54, 2, 47, 36];
+// function maxMin(arr) {
+//   let max = arr[0];
+//   let min = arr[0];
+
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < min) {
+//       min = arr[i];
+//     }
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+//   return { min, max };
+// }
+
+// console.log(maxMin(arr));
+
+//! Reverse array(manual method only)
+const arr = [12, 47, 54, 2, 47, 36];
